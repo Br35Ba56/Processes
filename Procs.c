ff97b95440d814a68d1e2ffeeaede651719c9525
Anthony Schneider <tonyschndr@gmail.com>
2017-11-03 04:20:00 +0000

    Homework 2 Process Project

--- Procs.c
+++ Procs.c
@@ -8,35 +8,49 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <signal.h>
+#include <unistd.h>
+#include <time.h>
 
 int main(int argc, const char * argv[]) {
-  int pid;
+  printf("Parent process is born, pid is: %d\n", getpid());
+  int parentID = getpid();
+  int pid, status;
   pid = fork();
+  
+  if (parentID == getpid()){//Put the parent to sleep so the first child process can start before the second child process
+    sleep(1);
+  }
+  
   switch (pid) {
     case -1:
       printf("Error creating child process\n");
       break;
     case 0:
       printf("First child is born, my pid is: %d\n", getpid());
-      for (int i = 0; i < 100; i++) {
-        printf("First child executes iteration %d of 100\n", i);
+      for (int i = 0; i < 10; i++) {
+        printf("First child executes iteration %d of 10\n", i);
         sleep(1);
-      };
-      printf("First child had died peacefully");
+      }
+      printf("First child had died peacefully\n");
+      exit(0);
       break;
     default:
-      printf("Parent process %d\n", getpid());
       pid = fork();
       if (pid == 0) {
-        printf("Child 2 created %d\n", getpid());
-        for (int i =0; i < 100; i++) {
-          printf("Second child executes iteration %d of 100\n", i);
+        printf("Second child is born, pid is: %d\n", getpid());
+        for (int i =0; i < 10; i++) {
+          printf("Second child executes iteration %d of 10\n", i);
           sleep(1);
         }
-        printf("Second child dies peacefully");
+        printf("Second child dies peacefully\n");
+	exit(0);
       }
+      wait(&status);//parent process will wait for the signal from the first child. 
+      wait(&status);//parent process will wait for the signal from the sercond child.
+	for (int i = 0; i < 10; i++) {//code the parent will execute.
+	  printf("Parent is now executing, pid : %d\n", getpid());
+	}
       break;
   }
   return 0;

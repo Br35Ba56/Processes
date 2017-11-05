54d94306ad86b01e7d043aa8a2685f23994bc581
Anthony Schneider <tonyschndr@gmail.com>
2017-11-05 00:44:30 +0000

    Final

--- Procs.c
+++ Procs.c
@@ -3,17 +3,20 @@
 //  Procs
 //
 //  Created by Anthony Schneider on 10/31/17.
-//  Copyright © 2017 Anthony Schneider. All rights reserved.
 //
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
-#include <time.h>
+#include <wait.h>
+
+//Functions
+void getProcesses();
 
 int main(int argc, const char * argv[]) {
   printf("Parent process is born, pid is: %d\n", getpid());
+  getProcesses();
   int parentID = getpid();
   int pid, status;
   pid = fork();
@@ -28,8 +31,9 @@ int main(int argc, const char * argv[]) {
       break;
     case 0:
       printf("First child is born, my pid is: %d\n", getpid());
+      getProcesses();
       for (int i = 0; i < 10; i++) {
-        printf("First child executes iteration %d of 10\n", i);
+        printf("First child executes iteration %d of 10\n", i+1);
         sleep(1);
       }
       printf("First child had died peacefully\n");
@@ -39,19 +43,33 @@ int main(int argc, const char * argv[]) {
       pid = fork();
       if (pid == 0) {
         printf("Second child is born, pid is: %d\n", getpid());
+        getProcesses();
         for (int i =0; i < 10; i++) {
-          printf("Second child executes iteration %d of 10\n", i);
+          printf("Second child executes iteration %d of 10\n", i+1);
           sleep(1);
         }
         printf("Second child dies peacefully\n");
 	exit(0);
+      } else {
+        if(getpid() != parentID) {
+          printf("Error creating second child process\n");
+        }
       }
       wait(&status);//parent process will wait for the signal from the first child. 
       wait(&status);//parent process will wait for the signal from the sercond child.
 	for (int i = 0; i < 10; i++) {//code the parent will execute.
-	  printf("Parent is now executing, pid : %d\n", getpid());
+	  printf("Parent is now executing\n");
 	}
+      getProcesses();
       break;
   }
+  printf("Parent process dies quietly\n");
   return 0;
 }
+//A simple function that uses the system to echo the processes from Procs
+void getProcesses(){
+  system("echo \"Processes running: \"");
+  system("pgrep Procs");
+}
+
+
